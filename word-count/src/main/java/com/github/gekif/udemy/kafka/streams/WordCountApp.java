fa8002423c83814ea9ccc33fecaf9d4af7769572
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-10-28 15:46:39 +0700

    Word Count Test Case

--- word-count/src/main/java/com/github/gekif/udemy/kafka/streams/WordCountApp.java
+++ word-count/src/main/java/com/github/gekif/udemy/kafka/streams/WordCountApp.java
@@ -6,21 +6,15 @@ import java.util.Arrays;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KStreamBuilder;
-import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
 
 public class WordCountApp {
-    public static void main(String[] args) {
-        Properties config = new Properties();
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
-        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-        KStreamBuilder builder = new KStreamBuilder();
+    public Topology createTopology() {
+        StreamsBuilder builder = new StreamsBuilder();
         // 1 - stream from Kafka
 
         KStream<String, String> textLines = builder.stream("word-count-input");
@@ -35,13 +29,27 @@ public class WordCountApp {
                 .selectKey((key, word) -> word)
                 // 5 - group by key before aggregation
                 .groupByKey()
-                // 6 - count occurrences
-                .count("Counts");
+                // 6 - count occurences
+                .count(Materialized.as("Counts"));
 
         // 7 - to in order to write the results back to kafka
-        wordCounts.to(Serdes.String(), Serdes.Long(), "word-count-output");
+        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
 
-        KafkaStreams streams = new KafkaStreams(builder, config);
+        return builder.build();
+    }
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+
+        WordCountApp wordCountApp = new WordCountApp();
+
+        KafkaStreams streams = new KafkaStreams(wordCountApp.createTopology(), config);
         streams.start();
 
 
