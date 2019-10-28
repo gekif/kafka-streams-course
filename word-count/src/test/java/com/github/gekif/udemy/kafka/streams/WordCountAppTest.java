a9555bbca78dfa53a3b0ce42d76fbe65e39a00f4
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-10-28 15:16:53 +0700

    Create Unit Test On Word Count App Kafka Stream

---
+++ word-count/src/test/java/com/github/gekif/udemy/kafka/streams/WordCountAppTest.java
@@ -0,0 +1,14 @@
+package com.github.gekif.udemy.kafka.streams;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WordCountAppTest {
+
+    @Test
+    public void dummyTest() {
+        String dummy = "Du" + "mmy";
+        assertEquals(dummy,"Dummy");
+    }
+}
